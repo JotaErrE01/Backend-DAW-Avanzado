6d65195f5426dcf7fcab5d362baf5d0aa1b5ecb0
jotaerre01 <jruiz967@gmail.com>
2022-09-06 13:25:24 -0500

    agregando docker compose para microservicios y  corrigiendo usuarios controller

--- ProyectoDAWBackend/Controllers/UsuariosController.cs
+++ ProyectoDAWBackend/Controllers/UsuariosController.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoDAWBackend.Data;
@@ -15,45 +20,45 @@ namespace ProyectoDAWBackend.Controllers {
 
     // GET: api/Usuarios
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Pregunta>>> GetPreguntas() {
-      if (_context.Preguntas == null) {
+    public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios() {
+      if (_context.Usuarios == null) {
         return NotFound();
       }
 
-      return await _context.Preguntas.ToListAsync();
+      return await _context.Usuarios.ToListAsync();
     }
 
     // GET: api/Usuarios/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<Pregunta>> GetPregunta(int id) {
-      if (_context.Preguntas == null) {
+    public async Task<ActionResult<Usuario>> GetUsuario(int id) {
+      if (_context.Usuarios == null) {
         return NotFound();
       }
 
-      var pregunta = await _context.Preguntas.FindAsync(id);
+      var usuario = await _context.Usuarios.FindAsync(id);
 
-      if (pregunta == null) {
+      if (usuario == null) {
         return NotFound();
       }
 
-      return pregunta;
+      return usuario;
     }
 
     // PUT: api/Usuarios/5
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutPregunta(int id, Pregunta pregunta) {
-      if (id != pregunta.Id) {
+    public async Task<IActionResult> PutUsuario(int id, Usuario usuario) {
+      if (id != usuario.Id) {
         return BadRequest();
       }
 
-      _context.Entry(pregunta).State = EntityState.Modified;
+      _context.Entry(usuario).State = EntityState.Modified;
 
       try {
         await _context.SaveChangesAsync();
       }
       catch (DbUpdateConcurrencyException) {
-        if (!PreguntaExists(id)) {
+        if (!UsuarioExists(id)) {
           return NotFound();
         }
         else {
@@ -67,37 +72,37 @@ namespace ProyectoDAWBackend.Controllers {
     // POST: api/Usuarios
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
-    public async Task<ActionResult<Pregunta>> PostPregunta(Pregunta pregunta) {
-      if (_context.Preguntas == null) {
-        return Problem("Entity set 'ApplicationDbContext.Preguntas'  is null.");
+    public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario) {
+      if (_context.Usuarios == null) {
+        return Problem("Entity set 'ApplicationDbContext.Usuarios'  is null.");
       }
 
-      _context.Preguntas.Add(pregunta);
+      _context.Usuarios.Add(usuario);
       await _context.SaveChangesAsync();
 
-      return CreatedAtAction("GetPregunta", new { id = pregunta.Id }, pregunta);
+      return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
     }
 
     // DELETE: api/Usuarios/5
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeletePregunta(int id) {
-      if (_context.Preguntas == null) {
+    public async Task<IActionResult> DeleteUsuario(int id) {
+      if (_context.Usuarios == null) {
         return NotFound();
       }
 
-      var pregunta = await _context.Preguntas.FindAsync(id);
-      if (pregunta == null) {
+      var usuario = await _context.Usuarios.FindAsync(id);
+      if (usuario == null) {
         return NotFound();
       }
 
-      _context.Preguntas.Remove(pregunta);
+      _context.Usuarios.Remove(usuario);
       await _context.SaveChangesAsync();
 
       return NoContent();
     }
 
-    private bool PreguntaExists(int id) {
-      return (_context.Preguntas?.Any(e => e.Id == id)).GetValueOrDefault();
+    private bool UsuarioExists(int id) {
+      return (_context.Usuarios?.Any(e => e.Id == id)).GetValueOrDefault();
     }
   }
 }
